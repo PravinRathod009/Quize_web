36e211b188eecc2e08a7411ef936ab1420c7362a
Pravin <pravinrathod22522@gmail.com>
2025-05-27 06:42:05 +0530

    add file

---
+++ server/config/database.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+
+
+ exports.connectToDB= async ()=>
+{
+     await mongoose.connect('mongodb://127.0.0.1:27017/Quizzy');
+     console.log('datebase connected..')
+}
+
+
+//exports.connectToDB
