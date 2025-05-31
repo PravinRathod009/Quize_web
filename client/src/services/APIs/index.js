5ae8b887b68577212bce220bb47f45e40a85820c
Pravin  Rathod <143316584+PravinRathod009@users.noreply.github.com>
2025-05-31 09:39:44 +0530

    Update index.j

--- client/src/services/APIs/index.js
+++ client/src/services/APIs/index.js
@@ -1,5 +1,5 @@
 // const BASE_URL = import.meta.env.VITE_BASE_URL;
- const BASE_URL = "http://localhost:4000/api/v1";
+ const BASE_URL = "https://quize-web-api-1.vercel.app/api/v1";
 
 export const authEndpoints = {
   SIGNUP: `${BASE_URL}/register`,
