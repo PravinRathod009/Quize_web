94ae2f4cc670cb3fc899ee954e052e0efdf707df
Pravin <pravinrathod22522@gmail.com>
2025-05-31 06:39:29 +0530

    done

--- client/src/services/APIs/index.js
+++ client/src/services/APIs/index.js
@@ -1,4 +1,5 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+// const BASE_URL = import.meta.env.VITE_BASE_URL;
+ const BASE_URL = "http://localhost:4000/api/v1";
 
 export const authEndpoints = {
   SIGNUP: `${BASE_URL}/register`,
--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,27 @@
-const mongoose=require('mongoose');
+// const mongoose=require('mongoose');
 
 
 
- exports.connectToDB= async ()=>
-{
-     await mongoose.connect('mongodb://127.0.0.1:27017/Quizzy');
-     console.log('datebase connected..')
-}
+//  exports.connectToDB= async ()=>
+// {
+//      await mongoose.connect('mongodb://127.0.0.1:27017/Quizzy');
+//      console.log('datebase connected..')
+// }
 
 
-//exports.connectToDB
+exports.connectToDB
+
+const mongoose = require("mongoose")
+require("dotenv").config();
+
+exports.connectToDB = () => {
+    mongoose.connect(process.env.DB_URL,{})
+    .then(() => {
+        console.log("Database connection successfull")
+    })
+    .catch((e) => {
+        console.log("Error occurred while connecting to DB")
+        console.error(e);
+        process.exit(1);
+    })
+} 
